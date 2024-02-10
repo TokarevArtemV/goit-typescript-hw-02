c5b882a440aca5d3226538cfdd44b693489e9534
TokarevArtemV <tokarev.artem.v@gmail.com>
2024-02-10 22:41:40 +0200

    finish

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,10 +3,12 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign(objA, objB);
 }
 
-const merged = merge({ name: "Alisa" }, { age: 28 });
+// не можу зрозуміти, чому я передаю масив, а тайпскрипт не видає помилку...
+
+const merged = merge(["name", "Alisa"], { age: 28 });
 
 export {};
