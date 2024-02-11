1f46dc069507c7a624f35c699df8674eb77b9f09
TokarevArtemV <tokarev.artem.v@gmail.com>
2024-02-11 22:09:46 +0200

    fix

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -3,6 +3,8 @@
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
 
-const myGender: "male" | "female" = "male";
+type Gender = "male" | "female";
+
+const myGender: Gender = "male";
 
 export {};
